4c1b66e33bcdae9b4e0f73930dc39864d710ed7f
Mahendiraprasath K <mahendiraprasath.k2024lcse@sece.ac.in>
2025-12-04 08:55:22 +0530

    day 3 tasks in this to check out it

---
+++ Sri-Eshwar-React-app-1/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import FlexBoxComponent from "./flex-box/flex-box-component";
+import HelloWorldComponent from "./hello-world/hello-world-component";
+import LeadersTomorrowComponent from "./leaders-tomorrow/leaders-tomorrow-component";
+import PostionComponent from "./positioning/position-component";
+import SymbolOfExcellenceComponent from "./symbol-of-excellence/symbol-of-execllence-component";
+
+function App() {
+  return (
+    <div>
+      {/* <HelloWorldComponent /> */}
+      {/* <FlexBoxComponent /> */}
+      {/* <LeadersTomorrowComponent /> */}
+      {/* <SymbolOfExcellenceComponent /> */}
+      <PostionComponent />
+    </div>
+  );
+}
+
+export default App;
