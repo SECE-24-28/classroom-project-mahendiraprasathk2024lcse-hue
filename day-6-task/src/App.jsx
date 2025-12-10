562ab0bac9eca1ba00614ec054daf1c677398802
Mahendiraprasath K <mahendiraprasath.k2024lcse@sece.ac.in>
2025-12-10 09:34:03 +0530

    Pushed the async folder with asynchronous code and api fetching

---
+++ day-6-task/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LoginComponent from "./login/login-component";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <LoginComponent />
+    </>
+  );
+}
+
+export default App;
